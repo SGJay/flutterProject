e4d1562797e3a30bc2a52c0d347c7f9253f18465
宋積杰 <zaq84723@gmail.com>
2020-02-26 22:16:06 +0800

    ImageViewer
    first add image

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -7,20 +7,126 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
-      home: RandomWords(),
+      title: 'flutter layout demo',
+      home: MainPage(),
     );
   }
 }
 
- class RandomWords extends StatefulWidget {
+class MainPage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState()=>  RandomWordsState();
+  State<StatefulWidget> createState() => MainPageState();
+}
 
+class MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: EdgeInsets.all(32),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Lake',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text('Switzerland',
+                    style: TextStyle(
+                      color: Colors.grey[500],
+                    ))
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+
+    Column _buildButtonColumn(Color color, IconData icon, String label) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            icon,
+            color: color,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                  fontSize: 12, fontWeight: FontWeight.w400, color: color),
+            ),
+          )
+        ],
+      );
+    }
+
+    var color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Image Viever'),
+        ),
+        body: ListView(
+          children: <Widget>[
+            Image.asset('images/lake.jpg',
+            width: 600,
+            height: 240,
+            fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ));
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => RandomWordsState();
 }
 
 class RandomWordsState extends State<RandomWords> {
-   List<WordPair> _suggestions = <WordPair>[];
+  List<WordPair> _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
